1f95ac154aba2449477f4220553939fbd8dbb3cf
Rafael Silva <rafael_rhps@outlook.com>
2023-04-04 10:32:45 -0300

    Componente Titulo concluído

---
+++ src/Components/Titulo/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const StyledTitulo = styled.h1`
+color:${({theme})=>theme.text};
+padding: 25px 0;
+`
+export default StyledTitulo
+
