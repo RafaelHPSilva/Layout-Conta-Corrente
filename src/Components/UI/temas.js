25030b25032d67350062a6f8d632d0ab393077df
Rafael Silva <rafael_rhps@outlook.com>
2023-04-04 10:37:19 -0300

    Componente UI temas concluído

---
+++ src/Components/UI/temas.js
@@ -0,0 +1,17 @@
+import { fundoClaro, conteudoClaro, textoFundoClaro, fundoEscuro, conteudoEscuro, textoFundoEscuro, filterClaro, filterEscuro} from "./Variaveis";
+
+
+export const temaClaro = {
+  body:fundoClaro,
+  inside: conteudoClaro,
+  text: textoFundoClaro,
+  filter: filterClaro
+}
+
+
+export const temaEscuro = {
+  body:fundoEscuro,
+  inside: conteudoEscuro,
+  text: textoFundoEscuro,
+  filter:filterEscuro
+}
